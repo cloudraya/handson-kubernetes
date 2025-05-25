c224262252ad7f0645acbed3e751cadac559b0d2
Mike <laguerdo@yahoo.com>
2025-05-25 13:26:20 +0700

    update create token

--- connect.sh
+++ connect.sh
@@ -1,8 +1,6 @@
 export KUBECONFIG=./kubeconfig.conf
 kubectl get nodes
 kubectl get namespaces
-kubectl create clusterrolebinding dashboard-admin-sa --clusterrole=cluster-admin --serviceaccount=default:dashboard-admin-sa
+kubectl -n kubernetes-dashboard create token kubernetes-dashboard
 
-kubectl apply -f dashboard-token.yaml
-kubectl -n default get secret dashboard-admin-sa-token -o go-template="{{.data.token | base64decode}}" && printf "\n"
 kubectl port-forward -n kubernetes-dashboard svc/kubernetes-dashboard 8443:443
